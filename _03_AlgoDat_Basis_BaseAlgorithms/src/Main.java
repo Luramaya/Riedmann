e64c57a71013d1c8701ca1154e331053a9b08a00
issa <larissa.mueller.k@gmail.com>
2022-10-10 18:52:25 +0200

    Kehrt Array aus Zahlen um

--- _03_AlgoDat_Basis_BaseAlgorithms/src/Main.java
+++ _03_AlgoDat_Basis_BaseAlgorithms/src/Main.java
@@ -1,27 +1,25 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Random;
+
 
 public class Main {
+
     public static void main(String[] args) {
-        int array[] = {20, 2, 1, 9, 5, 3, 5};
-        System.out.println("Anfangsarray:" + Arrays.toString(array));
-
-        int length = array.length;
-    public static int[] revert(int[] data){
-
-        }
-        for (int i = 0; i < length - 1; i++) {
-
-            if (array[i] > array[i + 1]) {
-                int num = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = num;
-                i = -1;
-            }
-        }
-        System.out.println("Endarray:" + Arrays.toString(array));
+        int[] data = {7, 9, 2, 31, 55, 22};
+        System.out.println("Anfangsarray:" + Arrays.toString(data));
+        revert(data);
+        System.out.println("Endarray:" + Arrays.toString(data));
 
     }
 
-}
+        public static int[] revert ( int[] data){
+
+            int length = data.length;
+            //tauscht eine Linke gegen eine Rechte Zahl aus der gleichen Position aus der Mitte gesehen
+            for (int i = 0; i < length / 2; i++) {
+                int num = data[i];
+                data[i] = data[length - (i + 1)];
+                data[length - (i + 1)] = num;
+            }
+            return data;
+        }
+    }
