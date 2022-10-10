73815c290935f15032ee57628727b8f7a1f2879b
issa <larissa.mueller.k@gmail.com>
2022-10-10 15:53:52 +0200

    Besserer Zahlen Sortierer mit nur einem Array

--- ZahlenSortierer/src/Main.java
+++ ZahlenSortierer/src/Main.java
@@ -1,41 +1,25 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-    }
+        int array[] = {10, 20, 12, 9, 5, 3, 55};
+        System.out.println("Anfangsarray:" + Arrays.toString(array));
 
-    public static ArrayList<Integer> sortList(ArrayList<Integer> inputList){
+        int length = array.length;
 
-        for (int i = inputList.size() - 1; i >= 0; i--) {
-            int currentInt = inputList.get(i);
+        for (int i = 0; i < length - 1; i++) {
 
-            for (int j = inputList.size() - 1; j >= 0 ; j--) {
-
-                int loopInt = inputList.get(j);
-
-                if (currentInt > inputList.get(j)){
-
-                }
-
-
+            if (array[i] > array[i + 1]) {
+                int num = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = num;
+                i = -1;
             }
-
-
-
         }
+        System.out.println("Endarray:" + Arrays.toString(array));
 
     }
-
-
-    public static int[] generateDataArray(int size){
-        int[] data = new int[size];
-        Random random = new Random();
-        for (int i = 0; i < data.length; i++) {
-            data[i] = random.nextInt(100);
-        }
-        return data;
-    }
-
 
 }
